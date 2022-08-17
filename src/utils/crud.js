08022b9dea2c2262d510a77d2169dcb859cc87b5
Bruno Rios <Bruno.Rios@endava.com>
2022-08-17 12:22:38 -0300

    Add controllers

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,56 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.params._id
 
-export const getMany = model => async (req, res) => {}
+  const doc = await model.findOne({ _id: id, user: userId })
 
-export const createOne = model => async (req, res) => {}
+  if (!doc) return res.status(404).end()
 
-export const updateOne = model => async (req, res) => {}
+  res.status(200).json({ data: doc })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user._id
+
+  const doc = await model.find({ createdBy: userId }).exec()
+
+  res.status(200).json({ data: doc })
+}
+
+export const createOne = model => async (req, res) => {
+  const doc = await model.create({
+    ...req.body,
+    createdBy: req.user._id
+  })
+
+  res.status(201).json({ data: doc })
+}
+
+export const updateOne = model => async (req, res) => {
+  const doc = await model.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      createdBy: req.user._id
+    },
+    req.body,
+    { new: true }
+  )
+
+  if (!doc) return res.status(400).end()
+
+  res.status(200).json({ data: doc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const doc = await model.findOneAndDelete({
+    _id: req.params.id,
+    createdBy: req.user._id
+  })
+
+  if (!doc) return res.status(400).end()
+
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
